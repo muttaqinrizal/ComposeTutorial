eb5d38ba8ef4c9fd915a3b8a8ec3a9b3d8c44fd6
muttaqinrizal <rizalm57@gmail.com>
2024-02-28 19:57:09 +0700

    add color schema

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -48,12 +50,13 @@ fun MessageCard(msg: Message){
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(text = msg.author, color = MaterialTheme.colorScheme.secondary)
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
