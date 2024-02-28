6ef79b6d6ed0d59d4dce068caeae5a70a8272789
muttaqinrizal <rizalm57@gmail.com>
2024-02-28 20:03:22 +0700

    enable dark mode

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -74,7 +75,13 @@ fun MessageCard(msg: Message){
 
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
 @Composable
 fun PreviewMessageCard(){
     ComposeTutorialTheme {
